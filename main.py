472db5957170a1195aa3c4f6139ce31baadf7f91
Nav <navdeepnx@gmail.com>
2024-02-22 15:50:57 -0800

    2nd api results show current lon/lat of iss

--- main.py
+++ main.py
@@ -31,3 +31,37 @@ g = geocoder.ip("me")
 file.write("\n Your current lat / long is: " + str(g.latlng))
 file.close()
 webbrowser.open("iss.txt")
+
+# setyp the world map in turtle module
+screen = turtle.Screen()
+#1280 = width, 720 = height
+screen.setup(1280, 720)
+
+# create the x, y coords of the 4 corners of the canvas so iss can freely
+# traverse the map and come back around to the other side
+screen.setworldcoordinates(-180, -90, 180, 90)
+
+# load the world map image
+
+screen.bgpic("map.gif")
+screen.register_shape("iss.gif")
+# Turtle() is the main class
+iss = turtle.Turtle()
+iss.shape("iss.gif")
+# give the pan shap an angle of 45 degrees when moving
+iss.setheading(45)
+iss.penup() # for no more drawing
+
+# input('stop')
+
+while True:
+    #load the current status of the ISS in real-time
+    # 2nd api
+    url = "http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+
+    #extract the ISS location
+    location = result["iss_position"]
+    lat = location["latitude"]
+    lon = location["longitude"]
