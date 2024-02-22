4a72a2ae328432af1535ed64f2011788a6922786
Nav <navdeepnx@gmail.com>
2024-02-22 15:14:48 -0800

    loops through all names from api and shows how many members on board currently

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ import time
 import webbrowser
 import geocoder
 
-# retrieve the name of all astronoughts on iss
-
-# url of api of current astronoughts
+# retrieve the name of all astronaut on iss
+# url of api of current astronaut
 url = "http://api.open-notify.org/astros.json"
+
+# use this url object to open the api
+response = urllib.request.urlopen(url)
+
+#reads the response brought from the api
+result = json.loads(response.read())
+
+file = open("iss.txt", "w")
+# str() wrap so we can concatanate the number into the string
+file.write("There are currently" +
+            str(result["number"]) + " astronauts on the ISS: \n\n")
+
+# get the name of the astronauts
+people = result["people"]
+for p in people:
+    file.write(p["name"] + " - on board" + "\n")
+
+
+# print our current longitude and langitude in txt file
