3cc42c7bed3a3eaaa4c5360cf072b2be3b1926bb
Nav <navdeepnx@gmail.com>
2024-02-22 15:02:36 -0800

    test commit, imported libraries and pip install geocoder

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import turtle
+import urllib.request
+import time
+import webbrowser
+import geocoder
+
+# retrieve the name of all astronoughts on iss
+
+# url of api of current astronoughts
+url = "http://api.open-notify.org/astros.json"
